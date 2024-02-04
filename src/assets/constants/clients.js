37225e6f8f2e3c9033e0e59d26fec60bc83c6807
Masroor Hamdani <masroor.hamdani@hashedin.com>
2024-02-04 16:34:32 +0530

    Updated our clients part

---
+++ src/assets/constants/clients.js
@@ -0,0 +1,560 @@
+export const client_tabs_list = [
+    {
+        id: "technology",
+        pid: 101,
+        title: "Technology"
+    },
+    {
+        id: "consumer_media",
+        pid: 102,
+        title: "Consumer & Media"
+    },
+    {
+        id: "financial",
+        pid: 103,
+        title: "Financial"
+    },
+    {
+        id: "legal_consulting",
+        pid: 104,
+        title: "Legal & Consulting"
+    },
+    {
+        id: "medical",
+        pid: 105,
+        title: "Medical"
+    },
+    {
+        id: "energy",
+        pid: 106,
+        title: "Energy"
+    },
+
+];
+
+export const clients_list = [
+    {
+        id: 1,
+        title: "atos",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc54016fa279b1c53c69129_atos.png'
+    },
+    {
+        id: 2,
+        title: "brocade",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc5499c29165d59505c0d17_brocade.png'
+    }, {
+        id: 3,
+        title: "century_link",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc54a3d1ade9783d29ebc4c_centurylink.png'
+    }, {
+        id: 4,
+        title: "cisco",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e7e57345a47696fba93c_Cisco.png'
+    }, {
+        id: 5,
+        title: "cision",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e8298ef2641a199998b9_Cision.png'
+    }, {
+        id: 6,
+        title: "cognizant",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e89a39e77a0abf69cf0c_cog.png'
+    },
+    {
+        id: 7,
+        title: "corning",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e90424651f1b2e05f6f8_corning.png'
+    },
+    {
+        id: 8,
+        title: "bimbo",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f7ff1c0a882f0d55928c_bimbo.png'
+    },
+    {
+        id: 9,
+        title: "comcast",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e8d540c5b022bdb02a47_comcast.png'
+    },
+    {
+        id: 10,
+        title: "disney",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ec45f8e1ba40f827051b_disney.png'
+    },
+    {
+        id: 11,
+        title: "foot",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fef0afdb3c640dff2778_Foot.png'
+    },
+    {
+        id: 12,
+        title: "lith",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f18d900d145a44d43f81_lith.png'
+    },
+    {
+        id: 13,
+        title: "metel",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f22aa4c9dd44921a17cf_metel.png'
+    },
+    {
+        id: 14,
+        title: "newsmax",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f379a9ebb77e202f3285_newsmax.png'
+    },
+    {
+        id: 15,
+        title: "nbc",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fec57228891e1ef27790_NBC.png'
+    },
+    {
+        id: 16,
+        title: "onstream",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f43def9a65e185c42f02_onstream.png'
+    },
+    {
+        id: 17,
+        title: "universal",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f65fde2d7a82c6a3a92b_uinv%20e%20buz.png'
+    },
+    {
+        id: 18,
+        title: "viacom",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f67a40c5b03363b076d5_viacom.png'
+    },
+    {
+        id: 19,
+        title: "worldpac",
+        link: '#',
+        category: "consumer_media",
+        desc: "Consumer & Media",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f7b69392de679533da98_Worldpac.png'
+    },
+    {
+        id: 20,
+        title: "covance",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7e92ca9ebb772a32f014e_covance.png'
+    },
+    {
+        id: 21,
+        title: "dell",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7eb89a4c9dd035619f71c_dell.png'
+    },
+    {
+        id: 22,
+        title: "didata",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ebe3de2d7a956ba379b1_Didata.png'
+    },
+    {
+        id: 23,
+        title: "drivenets",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ec9333416a4b164f744c_drivenets.png'
+    },
+    {
+        id: 24,
+        title: "genesis",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fca631a6bdaf0450f6d285f_Genesis.png'
+    },
+    {
+        id: 25,
+        title: "ibm",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f8d9f43a44a4b57a8356_IBM.png'
+    },
+    {
+        id: 26,
+        title: "globalgig",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f899f39476304c8b9d80_globalgig.png'
+    },
+    {
+        id: 27,
+        title: "lntinfotech",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7facdc2749130164f1ef7_L%26T%20infotech.png'
+    },
+    {
+        id: 28,
+        title: "predi",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ff7f0349173f3088ba97_predi.png'
+    },
+    {
+        id: 29,
+        title: "pitney",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f9917da4e76a3fd20e6d_pitney.png'
+    },
+    {
+        id: 30,
+        title: "shoretel",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f549b8c2af157a0dfd56_shortel.png'
+    },
+    {
+        id: 31,
+        title: "skyline",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fca63a9adcaae13b8dd1d3a_sky.png'
+    },
+    {
+        id: 32,
+        title: "ultimo",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fca63823a93e4590a80a4bc_ultimo.png'
+    },
+    {
+        id: 33,
+        title: "vstream",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f6d0afdb3c73b9fef286_vstream.png'
+    },
+    {
+        id: 34,
+        title: "walters",
+        link: '#',
+        category: "technology",
+        desc: "Technology",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f7640850fba734585aa2_walters.png'
+    },
+    {
+        id: 35,
+        title: "applebank",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc53e481ff0f0fbbefb234d_apple-bank.png'
+    },
+    {
+        id: 36,
+        title: "bofa",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fa60fe4aaa975a50e236_BOFA.png'
+    },
+    {
+        id: 37,
+        title: "barclays",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc547af70b20119e3b28b0c_barclays.png'
+    },
+    {
+        id: 38,
+        title: "citi",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc80094b9119a81f526d1ff_citi.png'
+    },
+    {
+        id: 39,
+        title: "credit",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc8006f3b35cea89d255cc9_credit.png'
+    },
+    {
+        id: 40,
+        title: "morgan",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc8000340edd0127a55a218_morgan.png'
+    },
+    {
+        id: 41,
+        title: "merell",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc8003ac27491ce254f399c_Merell.png'
+    },
+    {
+        id: 42,
+        title: "munre",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f2c3fa17e2bc1bbfd6c6_munre.png'
+    },
+    {
+        id: 43,
+        title: "nassuae",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ff1ef43a4481107aa980_nassuae.png'
+    },
+    {
+        id: 44,
+        title: "natwest",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f94916bddc2337ea78f3_natwest.png'
+    },{
+        id: 45,
+        title: "ncogroup",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f2ceedafd7246e5c7bcd_ncogroup.png'
+    },
+    {
+        id: 46,
+        title: "nyc",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f3c0b824b85f0a3b1edd_NYC%20.png'
+    },
+    {
+        id: 47,
+        title: "tdbank",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fb1fb697bc590a672455_TD%20Bank.png'
+    },
+    {
+        id: 48,
+        title: "tishman",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ffd4afdb3c6072ff2d19_tishman.png'
+    },
+    {
+        id: 49,
+        title: "ubs",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f5e6d459be48c1023754_UBS.png'
+    },
+    {
+        id: 50,
+        title: "visa",
+        link: '#',
+        category: "financial",
+        desc: "Financial",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fb94c995e66875dd46b4_VISA.png'
+    },
+    {
+        id: 51,
+        title: "dla",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f84d8045406263382060_DLA.png'
+    },
+    {
+        id: 52,
+        title: "ey",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ed0e5cb4a9a6785dfcb3_ey.png'
+    },
+    {
+        id: 53,
+        title: "frank",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f122d459bea838020f50_frank.png'
+    },
+    {
+        id: 54,
+        title: "marcum",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f1c24f0b8b6d48c69529_marcum.png'
+    },
+    {
+        id: 55,
+        title: "tcs",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f5ab4f0b8b30d8c6a1e0_TCS.png'
+    },
+    {
+        id: 56,
+        title: "wipro",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f71f8045400b5637f3da_wipro.png'
+    },
+    {
+        id: 57,
+        title: "prossk",
+        link: '#',
+        category: "legal_consulting",
+        desc: "Legal & Consulting",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f4a4b8c2af2f8a0df84f_prossk.png'
+    },
+    {
+        id: 58,
+        title: "becton",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc548488aae318e63306b64_becton.png'
+    },
+    {
+        id: 59,
+        title: "erie",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ecc7205b5d25c7287c08_erie.png'
+    },
+    {
+        id: 60,
+        title: "expressscripts",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7fa9375805c7fe8352bb0_Expressscripts.png'
+    },
+    {
+        id: 61,
+        title: "nuy",
+        link: '#',
+        ccategory: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f3fdd459be490702295d_NYU.png'
+    },
+    {
+        id: 62,
+        title: "pfizer",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f45f3d99a8aa1fff140c_Pfizer.png'
+    },
+    {
+        id: 63,
+        title: "mainline",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7ff50722889af99f278e5_main%20line.png'
+    },
+    {
+        id: 64,
+        title: "merek",
+        link: '#',
+        category: "medical",
+        desc: "Medical",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f910a9419828b532a0d6_Merek.png'
+    },
+    {
+        id: 65,
+        title: "bhp",
+        link: '#',
+        category: "energy",
+        desc: "Energy",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc548aae1e5802e7b76bc2f_BHP.png'
+    },
+    {
+        id: 65,
+        title: "pseg",
+        link: '#',
+        category: "energy",
+        desc: "Energy",
+        icon: 'https://assets.website-files.com/5ef224f47bfc26b19480bb6b/5fc7f50ec0c6eb3dcc84941c_PSEG.png'
+    }
+    
+];
